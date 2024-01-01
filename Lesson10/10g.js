0bf515e48a53fe13dd0cc2de3debef72a4abbf16
Keerthy Baby <keerthybaby025@gmail.com>
2024-01-01 23:15:43 +0530

    exercise:10g

---
+++ Lesson10/10g.js
@@ -0,0 +1,19 @@
+
+function toggleButton(selector){
+  const button = document.querySelector(selector);
+      if (!button.classList.contains('is-toggled')) {
+        turnOffPrevious()
+        button.classList.add('is-toggled');
+      } else {
+        button.classList.remove('is-toggled');
+      }
+}
+
+function turnOffPrevious(){
+  const previousButton=document.querySelector('.is-toggled');
+  if(previousButton){
+    previousButton.classList.remove('is-toggled');
+  }
+}
+      
+    
